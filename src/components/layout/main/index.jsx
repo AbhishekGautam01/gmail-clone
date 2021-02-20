8b46cfde6701fea2ef3f0c5a0d665453539f4005
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-02-20 21:42:48 +0530

    Side Icons Skeleton

--- src/components/layout/main/index.jsx
+++ src/components/layout/main/index.jsx
@@ -1,11 +1,13 @@
 import { Wrapper } from './styles/main';
 import Sidebar from '../sidebar';
 import EmailsView from '../emailView';
+import SideIcons from '../sideIcons';
 export default function Main() {
   return (
     <Wrapper>
       <Sidebar />
       <EmailsView />
+      <SideIcons />
     </Wrapper>
   );
 }
